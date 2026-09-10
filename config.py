85823830005b345a24e1c0a3bb26746f763ebca8
TOMIOKA567 <kp345193@gmail.com>
2026-09-10 10:36:43 +0530

    Update support channel and chat URLs in config.py
    Signed-off-by: TOMIOKA567 <kp345193@gmail.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fallenx")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/devilhome568")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+ksyhmX03BT5hZDVl")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
@@ -38,8 +38,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://carder.top/imagens/1786604011979-817800736.jpg")
+        self.START_IMG = getenv("START_IMG", "https://carder.top/imagens/1786604011979-817800736.jpg")
 
     def check(self):
         missing = [
