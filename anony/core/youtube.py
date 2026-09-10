f908721d6681f60d93bd56efc1ec8c5ea96e78f0
TOMIOKA567 <kp345193@gmail.com>
2026-09-10 14:41:36 +0530

    Update youtube.py
    Signed-off-by: TOMIOKA567 <kp345193@gmail.com>

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 AnonymousX1025
+# Licensed under the MIT License.
+# This file is part of AnonXMusic
+
+
 import os
 import re
 import yt_dlp
@@ -7,6 +12,7 @@ import aiohttp
 from pathlib import Path
 
 from py_yt import Playlist, VideosSearch
+
 from anony import logger
 from anony.helpers import Track, utils
 
@@ -20,7 +26,6 @@ class DummyLogger:
 
     def error(self, msg):
         pass
-
 
 class YouTube:
     def __init__(self):
@@ -46,29 +51,23 @@ class YouTube:
                 if file.endswith(".txt"):
                     self.cookies.append(f"{self.cookie_dir}/{file}")
             self.checked = True
-
         if not self.cookies:
             if not self.warned:
                 self.warned = True
                 logger.warning("Cookies are missing; downloads might fail.")
             return None
-
         return random.choice(self.cookies)
 
     async def save_cookies(self, urls: list[str]) -> None:
         logger.info("Saving cookies from urls...")
-
         async with aiohttp.ClientSession() as session:
             for url in urls:
                 name = url.split("/")[-1]
                 link = "https://batbin.me/raw/" + name
-
                 async with session.get(link) as resp:
                     resp.raise_for_status()
-
                     with open(f"{self.cookie_dir}/{name}.txt", "wb") as fw:
                         fw.write(await resp.read())
-
         logger.info(f"Cookies saved in {self.cookie_dir}.")
 
     def valid(self, url: str) -> bool:
@@ -77,18 +76,14 @@ class YouTube:
     def invalid(self, url: str) -> bool:
         return bool(re.match(self.iregex, url))
 
-    async def search(
-        self, query: str, m_id: int, video: bool = False
-    ) -> Track | None:
+    async def search(self, query: str, m_id: int, video: bool = False) -> Track | None:
         try:
             _search = VideosSearch(query, limit=1, with_live=False)
             results = await _search.next()
         except Exception:
             return None
-
         if results and results["result"]:
             data = results["result"][0]
-
             return Track(
                 id=data.get("id"),
                 channel_name=data.get("channel", {}).get("name"),
@@ -101,17 +96,12 @@ class YouTube:
                 view_count=data.get("viewCount", {}).get("short"),
                 video=video,
             )
-
         return None
 
-    async def playlist(
-        self, limit: int, user: str, url: str, video: bool
-    ) -> list[Track | None]:
+    async def playlist(self, limit: int, user: str, url: str, video: bool) -> list[Track | None]:
         tracks = []
-
         try:
             plist = await Playlist.get(url)
-
             for data in plist["videos"][:limit]:
                 track = Track(
                     id=data.get("id"),
@@ -125,12 +115,9 @@ class YouTube:
                     view_count="",
                     video=video,
                 )
-
                 tracks.append(track)
-
         except Exception:
             pass
-
         return tracks
 
     async def download(self, video_id: str, video: bool = False) -> str | None:
@@ -142,7 +129,6 @@ class YouTube:
             return filename
 
         cookie = self.get_cookies()
-
         base_opts = {
             "outtmpl": "downloads/%(id)s.%(ext)s",
             "quiet": True,
@@ -172,15 +158,11 @@ class YouTube:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 try:
                     ydl.download([url])
-                except (
-                    yt_dlp.utils.DownloadError,
-                    yt_dlp.utils.ExtractorError,
-                ):
+                except (yt_dlp.utils.DownloadError, yt_dlp.utils.ExtractorError):
                     return None
                 except Exception as ex:
                     logger.warning("Download failed: %s", ex)
                     return None
-
             return filename
 
         return await asyncio.to_thread(_download)
